f1957565ae470921d9531523aab59e96dc2451a7
Anatoli33 <hadjiev1215@gmail.com>
2026-03-26 10:07:11 +0200

    Create Error 404 page

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.js';
 import { Countdown } from './countdown/countdown.js';
 import { Counter } from './counter/counter.js';
+import { Error } from './error/error.js';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'counter', component: Counter},
-    {path: 'countdown', component: Countdown}
+    {path: 'countdown', component: Countdown},
+    {path: '**', component: Error}
 ];
